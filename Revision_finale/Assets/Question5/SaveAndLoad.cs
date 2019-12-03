30d01a77b6f4a34caf1ef5dd1ee5c1c4b6d2757b
B533P-20\533 <loucaluc@live.fr>
2019-12-03 11:25:40 -0500

    q5 terminé

--- Revision_finale/Assets/Question5/SaveAndLoad.cs
+++ Revision_finale/Assets/Question5/SaveAndLoad.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class SaveAndLoad : MonoBehaviour {
     PlayerAction playerAction;
@@ -8,20 +11,46 @@ public class SaveAndLoad : MonoBehaviour {
 
     private void Start() {
         playerAction = FindObjectOfType<PlayerAction>();
-        
     }
    
 	
 	
     public void SaveGame() {
-       
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "gameInfo.dat", FileMode.Create);
+        PlayerData playerDataToSave = new PlayerData();
 
+        playerDataToSave.cash = playerAction.GetAvailableCash();
+        playerDataToSave.oilNumber = playerAction.GetOilNumber();
+        playerDataToSave.goldNumber = playerAction.GetGoldNumber();
+
+        bf.Serialize(file, playerDataToSave);
+        file.Close();
 
     }
 
     public void LoadGame() {
+        BinaryFormatter bf = new BinaryFormatter();
 
-      
+        if (!File.Exists(Application.persistentDataPath + "gameInfo.dat"))
+        {
+            throw new Exception("Fichier du jeu innexistant");
+        }
 
+        FileStream file = File.Open(Application.persistentDataPath + "gameInfo.dat", FileMode.Open);
+        PlayerData playerDataToLoad = (PlayerData)bf.Deserialize(file);
+        file.Close();
+
+        playerAction.SetAvailableCash(playerDataToLoad.cash);
+        playerAction.SetGoldNumber(playerDataToLoad.goldNumber);
+        playerAction.SetOilNumber(playerDataToLoad.oilNumber);
+    }
+
+    [Serializable]
+    class PlayerData
+    {
+        public float cash;
+        public int oilNumber;
+        public int goldNumber;
     }
 }
